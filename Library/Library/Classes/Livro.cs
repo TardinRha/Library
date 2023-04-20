59a923e081aa0b4ce56cd259f0c9389a2afa5a0a
Rhaissa Tardin <rha.tardin@gmail.com>
2023-04-20 18:12:21 -0300

    inicio

---
+++ Library/Library/Classes/Livro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Classes
+{
+    public class Livro
+    {
+        public string Titulo { get; set; }
+        public string Subtitulo { get; set; }
+        public string Autor { get; set; } // vetor com split pq pode ser mais de 1. Separar por  ponto e virgula
+        public string Editora { get; set; }
+        public string NumeroEdicao { get; set; }
+        public string Categoria { get; set; } //split tbm
+        public string TextoApresentacao { get; set; }
+        public string? Coletanea { get; set; }
+        public string Volume { get; set; }
+        public string ISBN { get; set; }
+        public int Paginas { get; set; }
+        public int PaginasLidas { get; set; } = 0;
+
+        public Livro()
+        {
+        }
+
+        public Livro(string titulo, string subtitulo, string autor, string editora, string numeroEdicao, string categoria, string textoApresentacao, string? coletanea, string volume, string iSBN, int paginas)
+        {
+            Titulo = titulo;
+            Subtitulo = subtitulo;
+            Autor = autor;
+            Editora = editora;
+            NumeroEdicao = numeroEdicao;
+            Categoria = categoria;
+            TextoApresentacao = textoApresentacao;
+            Coletanea = coletanea;
+            Volume = volume;
+            ISBN = iSBN;
+            Paginas = paginas;
+        }
+
+    }
+}
---
+++ Library/Library/Classes/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Classes
+{
+    public class Usuario
+    {
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string nome, string login, string senha, DateTime dataNascimento)
+        {
+            Nome = nome;
+            Login = login;
+            Senha = senha;
+            DataNascimento = dataNascimento;
+        }
+    }
+}
