57599a95fab10cfc8fbe76dec9e215c003a2f24a
Rhaissa Tardin <rha.tardin@gmail.com>
2023-05-12 18:50:54 -0300

    iniciando nova branch

--- Library/Library/Classes/Livro.cs
+++ Library/Library/Classes/Livro.cs
@@ -8,63 +8,18 @@ namespace Library.Classes
 {
     public class Livro
     {
-        public string Titulo { get; set; }
-        public string Subtitulo { get; set; }
-        public string AutorPrincipal { get; set; }
-        public string Editora { get; set; }
-        public string NumeroEdicao { get; set; }
-        public string TextoApresentacao { get; set; }
-        public string? Colecao { get; set; }
-        public string Volume { get; set; }
+        public string ISBN10 { get; set; }
         public string ISBN13 { get; set; }
-        public string ID { get; set; }
-        public string StatusLivro { get; set; }
-        public string DataLancamento { get; set; }
-        public int Paginas { get; set; }
-        public List<string> OutrosAutores { get; set; } = new List<string>();
-        public List<string> Categoria { get; set; }
-
-        public Livro(string isbn, string titulo, string subtitulo, string autorPrincipal,
-            List<string> outrosAutores, string editora, string numeroEdicao, string dataLancamento,
-            List<string> categoria, string textoApresentacao, string colecao,
-            string volume, int paginas)
-        {
-            ISBN13 = isbn;
-            ID = isbn.Substring(0, 5);
-            Titulo = titulo;
-            Subtitulo = subtitulo;
-            AutorPrincipal = autorPrincipal;
-            OutrosAutores = outrosAutores;
-            Editora = editora;
-            NumeroEdicao = numeroEdicao;
-            DataLancamento = dataLancamento;
-            Categoria = categoria;
-            TextoApresentacao = textoApresentacao;
-            Colecao = colecao;
-            Volume = volume;
-            Paginas = paginas;
-        }
-        public void InformacaoLivro()
-        {
-            Console.WriteLine($"ID: {ID}");
-            Console.WriteLine($"{Titulo} - {Subtitulo}");
-            Console.Write($"Autor: {AutorPrincipal} - ");
-            foreach (var autor in OutrosAutores)
-            {
-                Console.Write(autor + ", ");
-            }
-            Console.WriteLine();
-            Console.Write($"Editora: {Editora} - Edição: {NumeroEdicao} - Categoria: ");
-            foreach (var categoria in Categoria)
-            {
-                Console.Write(categoria + ", ");
-            }
-            Console.WriteLine();
-            Console.WriteLine($"Coletânea: {Colecao} - Volume: {Volume} - Páginas: {Paginas}");
-            Console.WriteLine($"ISBN: {ISBN13}");
-            Console.WriteLine($"Sobre o livro: {TextoApresentacao}");
-            Console.WriteLine($"Status do livro: {StatusLivro}");
-            Console.WriteLine();
-        }
+        public string IdentificadorOpenLibrary { get; set; }
+        public string Title { get; set; }
+        public string PublishDate { get; set; }
+        public string PublishingCompany { get; set; }
+        public string PictureMedium { get; set; }
     }
+    public class Autor
+    {
+        public string URL { get; set; }
+        public string Name { get; set; }
+    }
+
 }
